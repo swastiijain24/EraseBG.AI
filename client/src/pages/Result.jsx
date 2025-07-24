8538898b8510287d4321b09be14a175d516a5f14
Swasti <swastiijain@gmail.com>
2025-07-24 22:52:27 +0530

    try another btn

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext.jsx'
+import { useNavigate } from 'react-router-dom'
 
 const Result = () => {
 
   const {resultImage, image}= useContext(AppContext)
+  const navigate = useNavigate();
 
   return (
     <div  className='mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh]'>
@@ -32,7 +34,7 @@ const Result = () => {
 
                 {/*-------- buttons --------*/}
                 { resultImage && <div className='flex justify-center sm:justify-end items-center flex-wrap gap-4 mt-6' >
-                  <button className='px-8 py-2.5 text-violet-600 text-sm border border-violet-600 rounded-full hover:scale-105 transition-all duration-700'> Try another image</button>
+                  <button onClick={()=> navigate('/')} className='px-8 py-2.5 text-violet-600 text-sm border border-violet-600 rounded-full hover:scale-105 transition-all duration-700'> Try another image</button>
                   <a href={resultImage} download className='px-8 py-2.5 text-white text-sm bg-gradient-to-r from-violet-600 to-fuchsia-500 rounded-full hover:scale-105 transition-all duration-700'>Download image</a>
                 </div>}
 
